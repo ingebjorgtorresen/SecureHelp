1d034d30ef301bb001bb4a2a5ce0abc56ac40af2
Ingebjørg Semb Tørresen <ingebsto@stud.ntnu.no>
2023-03-23 15:45:05 +0100

    #3 Setting access token lifetime to 15 minutes

--- backend/securehelp/settings.py
+++ backend/securehelp/settings.py
@@ -189,7 +189,7 @@ REST_FRAMEWORK = {
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60000),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': True,
 }
